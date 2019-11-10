9fcf427b4308b2ec3b913549d7a8ecba6dc630cd
= <jweir136@yahoo.com>
2019-11-10 10:50:51 -0500

    fix bugs and convert generators to grayscale to save mem.

--- overfiting_model.py
+++ overfiting_model.py
@@ -5,6 +5,12 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
+print("!"*40)
+print("\n")
+print("IMPORTED MODS")
+print("\n")
+print("!"*40)
+
 images_dir = "/storage/facial_expression_images/images"
 full_df = pd.read_csv("/storage/affectnet+expw.csv")
 large_subset, small_subset = train_test_split(full_df, test_size=0.05)
