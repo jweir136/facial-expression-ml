1f76e9f028f68d6cbf592475330f0f55875cb7dd
= <jweir136@yahoo.com>
2019-11-10 11:31:22 -0500

    init

---
+++ generator_test.py
@@ -0,0 +1,27 @@
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+generator = keras.ImageDataGenerator(rescale=1/255.)
+train_generator = generator.flow_from_dataframe(
+    directory="/storage/facial_expression_images/images",
+    dataframe="/storage/affectnet+expw.csv",
+    x_col='filenames',
+    y_col='expression',
+    target_size=(32, 32),
+    color_mode='grayscale',
+    class_mode='categorical'
+)
+validation_generator = generator.flow_from_dataframe(
+    directory="/storage/facial_expression_images/images",
+    dataframe="/storage/affectnet+expw.csv",
+    x_col="filenames",
+    y_col="expression",
+    target_size=(32, 32),
+    color_mode='grayscale',
+    class_mode='categorical'
+)
+
+for img_batch, target_batch in train_generator:
+    print(target_batch[0])
+    break
