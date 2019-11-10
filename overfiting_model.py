3932c36649982e855769b6a3108b67109630c8ec
= <jweir136@yahoo.com>
2019-11-10 07:32:33 -0500

    a

--- overfiting_model.py
+++ overfiting_model.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 images_dir = "/storage/facial_expression_images/images"
 full_df = pd.read_csv("/storage/affectnet+expw.csv")
-large_subset, small_subset = train_test_split(full_df, test_size=0.1)
+large_subset, small_subset = train_test_split(full_df, test_size=0.05)
 small_train_df, small_validation_df = train_test_split(small_subset, test_size=0.1)
 
 datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1/255.)
@@ -29,7 +29,12 @@ validation_generator = datagen.flow_from_dataframe(
 )
 
 model = keras.models.Sequential([
-    keras.layers.Conv2D(64, (3, 3), input_shape=(100, 100, 3), activation='relu'),
+    keras.layers.Conv2D(128, (3, 3), input_shape=(100, 100, 3), activation='relu'),
+    keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    keras.layers.Conv2D(128, (3, 3)),
+    keras.layers.MaxPooling2D((2, 2)),
+    keras.layers.Activation('relu'),
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
     keras.layers.Conv2D(64, (3, 3), activation='relu'),
     keras.layers.Conv2D(64, (3, 3)),
     keras.layers.MaxPooling2D((2, 2)),
@@ -41,6 +46,7 @@ model = keras.models.Sequential([
     keras.layers.Activation('relu'),
     keras.layers.Flatten(),
     keras.layers.Dropout(0.0),
+    keras.layers.Dense(128, activation='relu'),
     keras.layers.Dense(64, activation='relu'),
     keras.layers.Dense(32, activation='relu'),
     keras.layers.Dense(4, activation='softmax')
