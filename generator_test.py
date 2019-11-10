c772fa42d47f27ef57592af4a0963e00cabf0538
= <jweir136@yahoo.com>
2019-11-10 11:40:49 -0500

    init

--- generator_test.py
+++ generator_test.py
@@ -1,22 +1,16 @@
 import keras
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+full_df = pd.read_csv("/storage/affectnet+expw.csv")
 
 generator = keras.preprocessing.image.ImageDataGenerator(rescale=1/255.)
 train_generator = generator.flow_from_dataframe(
     directory="/storage/facial_expression_images/images",
-    dataframe="/storage/affectnet+expw.csv",
+    dataframe=full_df,
     x_col='filenames',
     y_col='expression',
-    target_size=(32, 32),
-    color_mode='grayscale',
-    class_mode='categorical',
-)
-validation_generator = generator.flow_from_dataframe(
-    directory="/storage/facial_expression_images/images",
-    dataframe="/storage/affectnet+expw.csv",
-    x_col="filenames",
-    y_col="expression",
     target_size=(32, 32),
     color_mode='grayscale',
     class_mode='categorical',
